9fa2b155735de1b5b49460d43251c8d023d1cc36
karthirajan <kajekarthi@gmail.com>
2019-05-31 08:33:30 +0530

    modified 3 class test scripts

--- src/ust/org/test/Life.java
+++ src/ust/org/test/Life.java
@@ -9,25 +9,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Life {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Greens-09\\git\\task\\driver\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.lifetime.life/");
-		List<WebElement> title1 = driver.findElements(By.xpath("//a[@class='dropdown-item header-dropdown-back']//parent::li[1]//parent::ul[1]"));
-		for (WebElement x : title1) {
-			System.out.println(x+"\n"+x.getText());
-			
-		}
-		List<WebElement> title2 = driver.findElements(By.xpath("//ul[@id='primaryNav']/li[1]/div[2]/ul/li[1]//following-sibling::li/a[1]"));
-		for (WebElement x : title2) {
-			System.out.println(x+"\n"+x.getText());
-			
 		
-	}
-	List<WebElement> title3= driver.findElements(By.xpath("//ul[@id='primaryNav']/li[1]/div[2]/ul/li[1]//following-sibling::li/a[contains(text(),'')]"));
-	for (WebElement x : title3) {
-		System.out.println(x+"\n"+x.getText());
 		
-	}
+		
+		System.out.println("removed all the scripts");
 	
 }
 }
